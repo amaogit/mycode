c96e14c9a19df1d1f947200ae3de41a05c503cdf
amaogit <maoaiwu@yahoo.com>
2023-01-26 05:31:28 +0000

    Learning to parse log files

--- attemptlogin/large_fileparse.py
+++ attemptlogin/large_fileparse.py
@@ -4,13 +4,15 @@
 loginfail = 0 # counter for fails
 
 # open the file for reading
-with open("/home/student/mycode/attemptlogin/keystone.common.wsgi") as kfile:
+keystone_file = open("/home/student/mycode/attemptlogin/keystone.common.wsgi","r")
 
-    # loop over the file
-    for line in kfile:
-        # if this 'fail pattern' appears in the line...
-        if "- - - - -] Authorization failed" in line:
-            loginfail += 1 # this is the same as loginfail = loginfail + 1
+# loop over the file
+for line in keystone_file:
+
+    # if this 'fail pattern' appears in the line...
+    if "- - - - -] Authorization failed" in line:
+        loginfail += 1 # this is the same as loginfail = loginfail + 1
 
 print("The number of failed log in attempts is", loginfail)
+keystone_file.close() # close the open file
 
