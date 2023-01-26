472a74a854abe9b2f556a35227d3600509fc6eec
amaogit <maoaiwu@yahoo.com>
2023-01-26 20:32:17 +0000

    test pylint

---
+++ smallscript.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+This program prints some things to the screen
+"""
+
+def main():
+    """Our main program"""
+    mynote = 'print this string' # define mynote
+    print(mynote)                # display our string
+
+main()
